7de620804953566ffc73a01b6a1e289385e5978d
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-08-03 22:45:10 +0200

    Open package

--- pkg/constants.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-const (
-	// Github defines the github provider based on the name passed to the command.
-	Github = "github"
-	// Gitlab defines the gitlab provider based on the name passed to the command.
-	Gitlab = "gitlab"
-)
--- pkg/gitlab/gitlab.go
+++
@@ -1,6 +0,0 @@
-package gitlab
-
-// ExtractRepoName gets the repo name from a payload.
-func ExtractRepoName(payload string) (string, error) {
-	return "", nil
-}
