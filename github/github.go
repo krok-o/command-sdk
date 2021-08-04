2e4d2536d326df648297f1e9ded7d5ba1c6152f8
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-08-04 08:05:02 +0200

    Parsing

--- github/github.go
+++ github/github.go
@@ -19,12 +19,38 @@ type Payload struct {
 	Repo Repository `json:"repository"`
 }
 
-// ExtractRepoName gets the repo name from a payload.
-func ExtractRepoName(payload string) (string, error) {
+// Parser parses the given payload and extracts information out of it.
+type Parser struct {
+	Payload Payload
+}
+
+// NewParser sets up the parser with the payload.
+func NewParser(payload string) (Parser, error) {
 	var p Payload
 	if err := json.Unmarshal([]byte(payload), &p); err != nil {
-		return "", err
+		return Parser{}, err
 	}
+	return Parser{
+		Payload: p,
+	}, nil
+}
 
-	return p.Repo.FullName, nil
+// RepoName gets the repo name from a payload.
+func (p Parser) RepoName() string {
+	return p.Payload.Repo.FullName
+}
+
+// GitURL gets the git url from a payload.
+func (p Parser) GitURL() string {
+	return p.Payload.Repo.GitURL
+}
+
+// SSHURL gets the ssh url from a payload.
+func (p Parser) SSHURL() string {
+	return p.Payload.Repo.SSHURL
+}
+
+// HTMLURL gets the html url from a payload.
+func (p Parser) HTMLURL() string {
+	return p.Payload.Repo.HTMLURL
 }
