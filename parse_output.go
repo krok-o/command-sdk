17f0ce5bc3c64b2a0684fbadd140475dc7828e81
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-10-30 09:31:38 +0100

    Added output

--- parse_output.go
+++ parse_output.go
@@ -2,16 +2,20 @@ package pkg
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 // Output formats some values as output to another command.
 // All output should be put on error.
-func Output(m map[string]string) string {
+func Output(m map[string]string) error {
 	var output []string
 	for k, v := range m {
 		output = append(output, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	return strings.Join(output, "\n")
+	if _, err := fmt.Fprint(os.Stderr, strings.Join(output, "\n")); err != nil {
+		return fmt.Errorf("failed to write to stderr: %w", err)
+	}
+	return nil
 }
