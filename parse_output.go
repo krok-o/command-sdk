9a332c18653a6c7d59427b5dd9fc6bebf18750fc
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-10-30 15:27:49 +0100

    Added command begin and end delimiter

--- parse_output.go
+++ parse_output.go
@@ -6,14 +6,21 @@ import (
 	"strings"
 )
 
+var (
+	BeginOutputFormat = "----- BEGIN OUTPUT -----"
+	EndOutputFormat   = "----- END OUTPUT -----"
+)
+
 // Output formats some values as output to another command.
 // All output should be put on error.
 func Output(m map[string]string) error {
-	var output []string
+	output := []string{
+		BeginOutputFormat,
+	}
 	for k, v := range m {
 		output = append(output, fmt.Sprintf("%s=%s", k, v))
 	}
-
+	output = append(output, EndOutputFormat)
 	if _, err := fmt.Fprint(os.Stderr, strings.Join(output, "\n")); err != nil {
 		return fmt.Errorf("failed to write to stderr: %w", err)
 	}
